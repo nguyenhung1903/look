aa17125679760fe47f1551282776e71ed85f631e
kunkka19xx <nobita079x@gmail.com>
2026-04-04 14:31:31 +0900

    Initial Commit

---
+++ look-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  look-app
+//
+//  Created by kunkka07xx on 2026/04/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ look-app/look_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  look_appApp.swift
+//  look-app
+//
+//  Created by kunkka07xx on 2026/04/04.
+//
+
+import SwiftUI
+
+@main
+struct look_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
